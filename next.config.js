3580c46538cad0a84072b22edad2291847376975
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2025-06-08 05:09:26 +0530

    deployement

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    domains: [],
+    remotePatterns: [
+      // {
+      //   protocol: 'https',
+      //   hostname: 'example.com',
+      //   pathname: '/images/**',
+      // },
+    ],
+  },
 };
 
 module.exports = nextConfig;
