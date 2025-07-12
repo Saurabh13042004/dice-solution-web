06a8c14ff23cedbb6ec8c51e1e0b9a9563696467
Harshit-1874 <harshit1084.be21@chitkarauniversity.edu.in>
2025-07-12 20:11:09 +0530

    ok

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -37,7 +37,7 @@ export function Navigation() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
-          <div className="flex-shrink-0 cursor-pointer p-4" onClick={() => navigateToPage('/')}>
+          <div className="flex-shrink-0 cursor-pointer pt-4" onClick={() => navigateToPage('/')}>
             <img src='/1-removebg-preview.png' alt='Logo' className="h-auto w-80" />
           </div>
 
